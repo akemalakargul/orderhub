40d8add4ddab96904b21ad6bfe445bead28049e7
Ahmet Kemal Akargul <ahmet.akargul@commencis.com>
2025-05-15 14:21:47 +0300

    init

---
+++ src/main/java/com/examplebroker/orderhub/BrokerOrderHubApplication.java
@@ -0,0 +1,13 @@
+package com.examplebroker.orderhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerOrderHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerOrderHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplebroker/orderhub/BrokerOrderHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplebroker.orderhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerOrderHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
